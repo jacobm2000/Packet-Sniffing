a35e251ed653b80d9b1d514f03cd9d8d4b1b888c
jacobm2000 <maurerja2000@gmail.com>
2025-04-09 14:06:15 -0400

    put output in pandas dataframe
    The output now goes to a pandas dataframe for neater storage and easier output. A timestamp was also added to the output to provide a more complete picure of the packets.

--- simpleSniffer.py
+++ simpleSniffer.py
@@ -1,6 +1,17 @@
 from scapy.all import *
+import pandas as pd
+from datetime import datetime
 pc=input("Do you want to choose a tcp port yes(Y)or no(N)\n") 
 num=input("How many packets do you want to capture \n") 
+table=pd.DataFrame(columns=['Time','src','dst'])
+pd.set_option('display.max_rows', None) ## allows all rows of the table to be printed to output
+
+#center aligns the column headers
+# Center-align the headers and data
+table.style.set_table_styles({
+    '': {'text-align': 'center'},  # Center the data
+    'th': {'text-align': 'center'}  # Center the headers
+})
 
 if(pc=="Y"):
     port= input("what tcp port you want to sniff \n")
@@ -15,8 +26,13 @@ for pkt in packets:
        if TCP in pkt:
             src_port = pkt[TCP].sport
             dst_port = pkt[TCP].dport
-            print(pkt[IP].src+ ":TCP "+ str(src_port) + " > "+pkt[IP].dst + ":TCP "+str(dst_port))
+            time=datetime.utcfromtimestamp(pkt[IP].time).strftime('%Y-%m-%d %H:%M:%S')
+            table.loc[len(table)]=[time,pkt[IP].src+ ":TCP ",pkt[IP].dst + ":TCP "+str(dst_port)]
+            
        elif UDP in pkt:
            src_port = pkt[UDP].sport
            dst_port = pkt[UDP].dport
-           print(pkt[IP].src+ ":UDP "+ str(src_port) + " > "+pkt[IP].dst +":UDP "+ str(dst_port))
+           time=datetime.utcfromtimestamp(pkt[IP].time).strftime('%Y-%m-%d %H:%M:%S')
+           table.loc[len(table)]=[time,pkt[IP].src+ ":UDP ",pkt[IP].dst + ":UDP "+str(dst_port)]
+          
+print(table)
